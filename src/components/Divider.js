f870d38afad24937df2e39480488a9f48be6ccdb
Ameera Jayakumar <ameeraj.mec@gmail.com>
2023-05-23 23:15:27 +0530

    sidenav

---
+++ src/components/Divider.js
@@ -0,0 +1,6 @@
+import { Divider } from '@mui/material';
+import { styled } from '@mui/system';
+
+export const CustomDivider = styled(Divider)(({ theme }) => ({
+  backgroundColor: '#394B61',
+}));
--- src/pages/global/Sidenav.js
+++ src/pages/global/Sidenav.js
@@ -1,5 +1,5 @@
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import { Box, Typography } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
 import 'react-pro-sidebar/dist/css/styles.css';
 import SearchIcon from '@mui/icons-material/Search';
 import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
@@ -10,13 +10,14 @@ import UpdateIcon from '@mui/icons-material/Update';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import { CustomDivider } from '../../components/Divider';
 
-const Item = ({ title, icon, selected }) => {
+const Item = ({ title, icon }) => {
   return (
     <MenuItem
+      disabled
       style={{
         color: '#D4D7DD',
-        cursor: 'none',
       }}
       icon={icon}
     >
@@ -38,31 +39,29 @@ const Sidenav = () => {
         '& .pro-inner-item': {
           padding: '5px 35px 5px 20px !important',
         },
-        '& .pro-inner-item:hover': {
-          color: '#00E0FF  !important',
-        },
       }}
     >
-      <ProSidebar>
+      <ProSidebar width="17rem">
         <Menu iconShape="square">
           {/* LOGO AND MENU ICON */}
-          <Box mb="25px">
+          <Box mt="19px" mb="20px">
             <Box display="flex" justifyContent="center" alignItems="center">
               <img
                 alt="profile-user"
                 width="100px"
                 height="100px"
                 src={`../../assets/profile-pic.png`}
-                style={{ cursor: 'pointer', borderRadius: '50%' }}
+                style={{ cursor: 'default', borderRadius: '50%' }}
               />
             </Box>
-            <Box textAlign="center">
-              <Typography variant="h5" color="green">
+            <Box textAlign="center" mt="15px">
+              <Typography fontSize="20px" color="#D4D7DD">
                 Eric Hoffman
               </Typography>
             </Box>
           </Box>
-          <Box paddingLeft="10%">
+          <CustomDivider />
+          <Box paddingLeft="10%" mb="12px" mt="16px">
             <MenuItem
               active="true"
               style={{
@@ -76,8 +75,14 @@ const Sidenav = () => {
             <Item title="Movie" icon={<LiveTvIcon />} />
             <Item title="TV Shows" icon={<DesktopWindowsOutlinedIcon />} />
             <Item title="My List" icon={<ListIcon />} />
+          </Box>
+          <CustomDivider />
+          <Box paddingLeft="10%" mt="10px" mb="12px">
             <Item title="Watch Later" icon={<UpdateIcon />} />
             <Item title="Recommended" icon={<FavoriteBorderIcon />} />
+          </Box>
+          <CustomDivider />
+          <Box paddingLeft="10%" mt="10px">
             <Item title="Settings" icon={<SettingsOutlinedIcon />} />
             <Item title="Logout" icon={<LogoutOutlinedIcon />} />
           </Box>
