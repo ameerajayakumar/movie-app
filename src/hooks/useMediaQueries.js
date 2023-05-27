a711a402913324c6fc6eaf7c4f33f79d4384339b
Ameera Jayakumar <ameeraj.mec@gmail.com>
2023-05-27 20:19:54 +0530

    screen width- column calculation

--- src/hooks/useMediaQueries.js
+++ src/hooks/useMediaQueries.js
@@ -1,4 +1,30 @@
 import { useEffect, useState } from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+export const useWidth = () => {
+  const xxl = useMediaQuery((theme) => theme.breakpoints.up(2000));
+  const xl = useMediaQuery((theme) => theme.breakpoints.between(1600, 2000));
+  const lg = useMediaQuery((theme) => theme.breakpoints.between(1201, 1600));
+  const md = useMediaQuery((theme) => theme.breakpoints.between(900, 1200));
+  const sm = useMediaQuery((theme) => theme.breakpoints.between(701, 900));
+  const xs = useMediaQuery((theme) => theme.breakpoints.between(0, 700));
+  switch (true) {
+    case xxl:
+      return 6;
+    case xl:
+      return 5;
+    case lg:
+      return 4;
+    case md:
+      return 3;
+    case sm:
+      return 2;
+    case xs:
+      return 1;
+    default:
+      return 1;
+  }
+};
 
 const deviceType = (size, device) => {
   switch (true) {
--- src/pages/discover/Discover.js
+++ src/pages/discover/Discover.js
@@ -1,30 +1,21 @@
 import { Typography } from '@mui/material';
 import MovieCard from '../../components/MovieCard';
 import ExpandCard from '../../components/ExpandCard';
-import { useEffect, useState } from 'react';
-import useMediaQueries from '../../hooks/useMediaQueries';
+import { Fragment, useState } from 'react';
+import useMediaQueries, { useWidth } from '../../hooks/useMediaQueries';
 import Grid from '@mui/material/Unstable_Grid2';
 
 const Discover = ({ movieList }) => {
   const isMobile = useMediaQueries('mobile');
   const [open, setOpen] = useState(false);
   const [movieDetails, setMovieDetails] = useState([]);
-  const column = 5;
+  const column = useWidth();
   const rows = Math.ceil(movieList.length / column);
   const gridRows = Array.from({ length: rows });
   const [selectedMovie, setSelectedMovie] = useState();
 
   const selectMovie = (rowIndex, movieIndex) => {
     const currentMovie = movieList[rowIndex === 0 ? movieIndex : rowIndex * column + movieIndex];
-    // if (selectedMovie) {
-    //   if (selectedMovie.movie.imdbID === currentMovie.imdbID && open === true) setOpen(false);
-    //   else {
-    //     setOpen(false);
-    //     setTimeout(() => {
-    //       setOpen(true);
-    //     }, 80);
-    //   }
-    // }
     setSelectedMovie({
       row: rowIndex,
       index: movieIndex,
@@ -34,57 +25,49 @@ const Discover = ({ movieList }) => {
 
   return (
     <>
-      {movieList.length === 0 && (
+      {movieList.length > 0 ? (
+        <Grid container justifyContent={isMobile ? 'center' : ''}>
+          {gridRows.map((_, rowIndex) => {
+            const moviesRow = movieList.slice(rowIndex * column, (rowIndex + 1) * column);
+            const show = selectedMovie && selectedMovie['row'] !== undefined && selectedMovie['row'] === rowIndex;
+            return (
+              <Fragment key={rowIndex}>
+                <ExpandCard movie={movieDetails} open={show} />
+                <Grid
+                  key={`row-${rowIndex}`}
+                  container
+                  xs={12}
+                  my="2rem"
+                  mx="1rem"
+                  justifyContent={moviesRow.length < column ? '' : moviesRow.length === 1 ? 'center' : 'space-between'}
+                >
+                  {moviesRow.map((movie, movieIndex) => {
+                    return (
+                      <Grid xl={12 / column} sx={{ width: 'fit-content' }} key={movie.imdbID} display="flex" justifyContent="center">
+                        <MovieCard
+                          movie={movie}
+                          open={open}
+                          setOpen={setOpen}
+                          setMovieDetails={setMovieDetails}
+                          prevMovie={movieDetails}
+                          index={movieIndex}
+                          clicked={selectedMovie && selectedMovie.movie.imdbID === movie.imdbID}
+                          setClicked={() => selectMovie(rowIndex, movieIndex)}
+                          selectedMovie={selectedMovie}
+                        />
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </Fragment>
+            );
+          })}
+        </Grid>
+      ) : (
         <Typography component="div" fontSize={isMobile ? '15px' : '21px'} fontWeight="600" mt="2.5rem" ml="3rem" color="#fff">
           No results found for your search.
         </Typography>
       )}
-      <Grid container justifyContent={isMobile ? 'center' : ''}>
-        {gridRows.map((_, rowIndex) => {
-          const moviesRow = movieList.slice(rowIndex * column, (rowIndex + 1) * column);
-          const show = selectedMovie && selectedMovie['row'] !== undefined && selectedMovie['row'] === rowIndex;
-          return (
-            <>
-              <ExpandCard movie={movieDetails} open={show} />
-              <Grid
-                key={`row-${rowIndex}`}
-                container
-                xs={12}
-                my="2rem"
-                mx="1rem"
-                justifyContent={moviesRow.length < column ? '' : moviesRow.length === 1 ? 'center' : 'space-between'}
-              >
-                {moviesRow.map((movie, movieIndex) => {
-                  return (
-                    <Grid
-                      xl={12 / column}
-                      lg={15 / column}
-                      md={20 / column}
-                      xs={25 / column}
-                      sx={{ width: 'fit-content' }}
-                      key={movie.imdbID}
-                      display="flex"
-                      justifyContent="center"
-                    >
-                      <MovieCard
-                        movie={movie}
-                        open={open}
-                        setOpen={setOpen}
-                        setMovieDetails={setMovieDetails}
-                        prevMovie={movieDetails}
-                        index={movieIndex}
-                        clicked={selectedMovie && selectedMovie.movie.imdbID === movie.imdbID}
-                        setClicked={() => selectMovie(rowIndex, movieIndex)}
-                        selectedMovie={selectedMovie}
-                      />
-                    </Grid>
-                  );
-                })}
-              </Grid>
-            </>
-          );
-        })}
-      </Grid>
     </>
   );
 };
